7785d820e78f05b075d362aa05834c940be849ab
MathieuJack <products@yadusurf.com>
2019-11-17 14:43:32 +0100

    Update Program.cs
    code was edited in github

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // Code was edited in GitHub
             Console.WriteLine("Hello World!");
         }
     }
